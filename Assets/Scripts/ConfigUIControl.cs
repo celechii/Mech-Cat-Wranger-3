4ad7bd4f2e4a7837778b165d795ad5c6bdcbe14a
Noé <kathryncharron@cmail.carleton.ca>
2020-12-06 23:01:14 -0500

    key shit stuff before i break everything :))))

--- Assets/Scripts/ConfigUIControl.cs
+++ Assets/Scripts/ConfigUIControl.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 
 public class ConfigUIControl : MonoBehaviour {
 
-	public Color keyColour;
+	public Color keyTextColour;
+	public Color keyBackgroundDefaultColour;
+	public Color keyBackgroundBoundColour;
+	public Color keyBackgroundBrokenColour;
 	[Header("//BUTTONS")]
 	public TextMeshProUGUI walkForwards;
 	public TextMeshProUGUI walkBackwards;
@@ -17,36 +20,78 @@ public class ConfigUIControl : MonoBehaviour {
 	public TextMeshProUGUI hold;
 	public TextMeshProUGUI soothe;
 	public TextMeshProUGUI toggleDoors;
+	[Header("//KEYS")]
+	public Image[] keyBGs;
 
 	private Canvas canvas;
 	private TextMeshProUGUI currentButtonText;
 	private string currentKeyName;
-	private HashSet<KeyCode> validKeys;
+	private HashSet<KeyCode> validKeysHash;
+	private List<KeyCode> validKeys;
+	private List<KeyCode> brokenKeys;
 
 	private void Awake() {
-		validKeys = new HashSet<KeyCode>() {
+		validKeys = new List<KeyCode>() {
 			KeyCode.BackQuote,
+				KeyCode.Alpha1,
+				KeyCode.Alpha2,
+				KeyCode.Alpha3,
+				KeyCode.Alpha4,
+				KeyCode.Alpha5,
+				KeyCode.Alpha6,
+				KeyCode.Alpha7,
+				KeyCode.Alpha8,
+				KeyCode.Alpha9,
+				KeyCode.Alpha0,
 				KeyCode.Minus,
 				KeyCode.Equals,
 				KeyCode.Backspace,
 				KeyCode.Tab,
+				KeyCode.Q,
+				KeyCode.W,
+				KeyCode.E,
+				KeyCode.R,
+				KeyCode.T,
+				KeyCode.Y,
+				KeyCode.U,
+				KeyCode.I,
+				KeyCode.O,
+				KeyCode.P,
 				KeyCode.LeftBracket,
 				KeyCode.RightBracket,
 				KeyCode.Backslash,
+				KeyCode.A,
+				KeyCode.S,
+				KeyCode.D,
+				KeyCode.F,
+				KeyCode.G,
+				KeyCode.H,
+				KeyCode.J,
+				KeyCode.K,
+				KeyCode.L,
 				KeyCode.Semicolon,
 				KeyCode.Quote,
 				KeyCode.Return,
 				KeyCode.LeftShift,
+				KeyCode.Z,
+				KeyCode.X,
+				KeyCode.C,
+				KeyCode.V,
+				KeyCode.B,
+				KeyCode.N,
+				KeyCode.M,
 				KeyCode.Less,
 				KeyCode.Greater,
 				KeyCode.Slash,
 				KeyCode.RightShift,
-				KeyCode.Space
+				KeyCode.Space,
+				KeyCode.LeftArrow,
+				KeyCode.UpArrow,
+				KeyCode.DownArrow,
+				KeyCode.RightArrow
 		};
-		for (int i = (int)KeyCode.Alpha0; i <= (int)KeyCode.Alpha9; i++)
-			validKeys.Add((KeyCode)i);
-		for (int i = (int)KeyCode.A; i <= (int)KeyCode.Z; i++)
-			validKeys.Add((KeyCode)i);
+		validKeysHash = new HashSet<KeyCode>(validKeys);
+		brokenKeys = new List<KeyCode>();
 	}
 
 	private void Start() {
@@ -60,6 +105,8 @@ public class ConfigUIControl : MonoBehaviour {
 		UpdateText(hold, "Hold");
 		UpdateText(soothe, "Soothe");
 		UpdateText(toggleDoors, "Toggle Doors");
+
+		UpdateKeyBGs();
 	}
 
 	public void ShowConfig() {
@@ -70,8 +117,30 @@ public class ConfigUIControl : MonoBehaviour {
 		canvas.enabled = false;
 	}
 
+	public void BreakKey(KeyCode key) {
+		brokenKeys.Add(key);
+		validKeysHash.Remove(key);
+		UpdateKeyBGs();
+	}
+
 	public void RebindControl(string control) {
 		StartCoroutine(Rebind(control));
+	}
+
+	public void UpdateKeyBGs() {
+		for (int i = 0; i < keyBGs.Length; i++) {
+			KeyCode key = validKeys[i];
+			if (brokenKeys.Contains(key))
+				keyBGs[i].color = keyBackgroundBrokenColour;
+			else if (InputManager.IsKeyAlreadyBound(key))
+				keyBGs[i].color = keyBackgroundBoundColour;
+			else
+				keyBGs[i].color = keyBackgroundDefaultColour;
+		}
+
+		foreach (KeyCode key in brokenKeys)
+			keyBGs[validKeys.IndexOf(key)].color = keyBackgroundBrokenColour;
+
 	}
 
 	public IEnumerator Rebind(string control) {
@@ -103,8 +172,9 @@ public class ConfigUIControl : MonoBehaviour {
 			print($"fuck, lookin for {control}");
 		currentKeyName = control;
 
-		currentButtonText.text = GetText(currentButtonText.gameObject.name, "...");
-		InputManager.RebindKey(control, UpdateText, false, false, validKeys);
+		currentButtonText.text = GetText(currentButtonText.gameObject.name, "<size=9><ESC TO CANCEL></size>");
+		InputManager.RebindKey(control, UpdateText, false, false, validKeysHash);
+		UpdateKeyBGs();
 	}
 
 	private void UpdateText() => UpdateText(currentButtonText, currentKeyName);
@@ -114,6 +184,6 @@ public class ConfigUIControl : MonoBehaviour {
 	}
 
 	private string GetText(string name, string input) {
-		return $"{name} | {input.Colour(keyColour)}";
+		return $"{name} | {input.Colour(keyTextColour)}";
 	}
 }
